43b2b25c18970fccba11747f9ade0cfef8af0e0b
Dedger3 <dmytro.zadorozhnyi@nure.ua>
2021-10-30 03:44:22 +0300

    change on 3:44

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=["start"])
 def handle_message(message):
-    bot.reply_to(message, "Привет Друг!")
+    bot.reply_to(message, "Привет Друг! Рад тебя видеть!")
 
 @bot.message_handler(content_types=["text"])
 def handle_message(message):
